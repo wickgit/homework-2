a2b10d548bb6112db545cfbf0860640eaa7b1d5b
Markiyan <melnik.markiyan@gmail.com>
2023-06-20 21:56:55 +0300

    + getFighterDetails function

--- src/javascript/services/fightersService.js
+++ src/javascript/services/fightersService.js
@@ -3,6 +3,8 @@ import callApi from '../helpers/apiHelper';
 class FighterService {
     #endpoint = 'fighters.json';
 
+    #fighterEndpoint = 'details/fighter/';
+
     async getFighters() {
         try {
             const apiResult = await callApi(this.#endpoint);
@@ -15,6 +17,8 @@ class FighterService {
     async getFighterDetails(id) {
         // todo: implement this method
         // endpoint - `details/fighter/${id}.json`;
+        const fighterDetails = await callApi(`${this.#fighterEndpoint}/${id}.json`);
+        return fighterDetails;
     }
 }
 
